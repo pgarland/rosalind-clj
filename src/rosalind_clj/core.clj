11748b7b95cf19d0899ed5ce50e394e792b3c634
Phillip Garland <pgarland@gmail.com>
2013-01-27 15:22:55 -0800

    Added translate function

--- src/rosalind_clj/core.clj
+++ src/rosalind_clj/core.clj
@@ -138,3 +138,29 @@ list of ids with that prefix"
                  (* (/ hr indv)
                     (/ (dec hr) (dec indv))
                     1))))))
+
+(def transl-map {"UUU" \F,      "CUU" \L,      "AUU" \I,      "GUU" \V
+                 "UUC" \F,      "CUC" \L,      "AUC" \I,      "GUC" \V,
+                 "UUA" \L,      "CUA" \L,      "AUA" \I,      "GUA" \V,
+                 "UUG" \L,      "CUG" \L,      "AUG" \M,      "GUG" \V,
+                 "UCU" \S,      "CCU" \P,      "ACU" \T,      "GCU" \A,
+                 "UCC" \S,      "CCC" \P,      "ACC" \T,      "GCC" \A,
+                 "UCA" \S,      "CCA" \P,      "ACA" \T,      "GCA" \A,
+                 "UCG" \S,      "CCG" \P,      "ACG" \T,      "GCG" \A,
+                 "UAU" \Y,      "CAU" \H,      "AAU" \N,      "GAU" \D,
+                 "UAC" \Y,      "CAC" \H,      "AAC" \N,      "GAC" \D
+                 "UAA" nil,   "CAA" \Q,      "AAA" \K,      "GAA" \E,
+                 "UAG" nil,   "CAG" \Q,      "AAG" \K,      "GAG" \E,
+                 "UGU" \C,      "CGU" \R,      "AGU" \S,      "GGU" \G,
+                 "UGC" \C,      "CGC" \R,      "AGC" \S,      "GGC" \G,
+                 "UGA" nil,   "CGA" \R,      "AGA" \R,      "GGA" \G,
+                 "UGG" \W,      "CGG" \R,      "AGG" \R,      "GGG" \G})
+
+(defn translate [rna]
+  "Returns a lazy-sequence of amino acids translated from RNA"
+  (lazy-seq 
+   (if-let [[x y z & xs] rna]
+     (cons (transl-map (str/join (list x y z)))
+           (translate xs))
+     ())))
+       
