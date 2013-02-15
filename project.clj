d7a41f120952af91bc6da73278a4155aeebb03c9
Phillip Garland <pgarland@gmail.com>
2013-02-15 11:12:27 -0800

    Switch ring/compojure dependency order to allow use of newer ring

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.5"]
                  [ring "1.1.8"]
+                 [compojure "1.1.5"]
                  [hiccup "1.0.2"]]
     :plugins [[lein-ring "0.8.2" :exclusions [org.clojure/clojure]]]
     :ring {:handler hello-world.handler/app}
