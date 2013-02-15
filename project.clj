971ff35a7bc6f4ee3410d144b3ab185f56e0a253
Phillip Garland <pgarland@gmail.com>
2013-02-15 11:10:36 -0800

    Add ring and compojure dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "http://github.com/pgarland/rosalind-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]
+                 [ring "1.1.8"]
+                 [hiccup "1.0.2"]]
+    :plugins [[lein-ring "0.8.2" :exclusions [org.clojure/clojure]]]
+    :ring {:handler hello-world.handler/app}
+    :profiles
+    {:dev {:dependencies [[ring-mock "0.1.3"]
+                          [ring-server "0.2.7"]]}})
